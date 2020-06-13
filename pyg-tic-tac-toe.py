58c1911892df8044d04740c0b62339f105fb001e
Chad Larson <techchad2@gmail.com>
2020-06-12 23:54:12 -0500

    Added mouse clicks to add X

--- pyg-tic-tac-toe.py
+++ pyg-tic-tac-toe.py
@@ -1,6 +1,7 @@
 # tic tac toe game using pygame
 import pygame
 
+from typing import Tuple
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 500
@@ -34,6 +35,27 @@ def draw_o(win: pygame.Surface, x: int, y: int) -> None:
     pygame.draw.circle(win, O_COLOR, (x, y), 48, 1)
 
 
+def get_grid_location(x: int, y: int) -> Tuple[int, int]:
+    if 10 <= x <= 132 and 10 <= y <= 135:
+        return POS_1
+    elif 135 <= x <= 263 and 10 <= y <= 135:
+        return POS_2
+    elif 266 <= x <= 390 and 10 <= y <= 135:
+        return POS_3
+    elif 10 <= x <= 132 and 135 <= y <= 263:
+        return POS_4
+    elif 135 <= x <= 263 and 135 <= y <= 263:
+        return POS_5
+    elif 266 <= x <= 390 and 135 <= y <= 263:
+        return POS_6
+    elif 10 <= x <= 132 and 266 <= y <= 390:
+        return POS_7
+    elif 135 <= x <= 263 and 266 <= y <= 390:
+        return POS_8
+    elif 266 <= x <= 390 and 266 <= y <= 390:
+        return POS_9
+
+
 def game_loop(win: pygame.Surface) -> None:
     run = True
 
@@ -44,6 +66,11 @@ def game_loop(win: pygame.Surface) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                grid_pos = get_grid_location(mouse_x, mouse_y)
+                if grid_pos:
+                    draw_x(win, *grid_pos)
 
 
 def main() -> None:
