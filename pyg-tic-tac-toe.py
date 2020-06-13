6e950514ac6e03d725861106d9978183364170d0
Chad Larson <techchad2@gmail.com>
2020-06-13 11:30:43 -0500

    basic game play

--- pyg-tic-tac-toe.py
+++ pyg-tic-tac-toe.py
@@ -1,22 +1,17 @@
 # tic tac toe game using pygame
 import pygame
 
-from typing import Tuple
-
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 500
 GRID_COLOR = (128, 128, 180)
 X_COLOR = (190, 0, 255)
 O_COLOR = (0, 255, 128)
-POS_1 = (67, 67)
-POS_2 = (202, 67)
-POS_3 = (337, 67)
-POS_4 = (67, 202)
-POS_5 = (202, 202)
-POS_6 = (337, 202)
-POS_7 = (67, 337)
-POS_8 = (202, 337)
-POS_9 = (337, 337)
+WINNER_COLOR = (255, 255, 255)
+STALEMATE_COLOR = (255, 0, 0)
+GAME_OVER_COLOR = (255, 0, 0)
+POS_DICT = {1: (67, 67), 2: (202, 67), 3: (337, 67),
+            4: (67, 202), 5: (202, 202), 6: (337, 202),
+            7: (67, 337), 8: (202, 337), 9: (337, 337)}
 
 
 def draw_grid(win: pygame.Surface) -> None:
@@ -35,33 +30,75 @@ def draw_o(win: pygame.Surface, x: int, y: int) -> None:
     pygame.draw.circle(win, O_COLOR, (x, y), 48, 1)
 
 
-def get_grid_location(x: int, y: int) -> Tuple[int, int]:
+def get_grid_location(x: int, y: int) -> int:
     if 10 <= x <= 132 and 10 <= y <= 135:
-        return POS_1
+        return 1
     elif 135 <= x <= 263 and 10 <= y <= 135:
-        return POS_2
+        return 2
     elif 266 <= x <= 390 and 10 <= y <= 135:
-        return POS_3
+        return 3
     elif 10 <= x <= 132 and 135 <= y <= 263:
-        return POS_4
+        return 4
     elif 135 <= x <= 263 and 135 <= y <= 263:
-        return POS_5
+        return 5
     elif 266 <= x <= 390 and 135 <= y <= 263:
-        return POS_6
+        return 6
     elif 10 <= x <= 132 and 266 <= y <= 390:
-        return POS_7
+        return 7
     elif 135 <= x <= 263 and 266 <= y <= 390:
-        return POS_8
+        return 8
     elif 266 <= x <= 390 and 266 <= y <= 390:
-        return POS_9
+        return 9
+
+
+def check_for_winner(game_board: list) -> str:
+    for i in range(0, 9, 3):
+        if game_board[i] == game_board[i + 1] == game_board[i + 2] == "X":
+            return "X"
+        elif game_board[i] == game_board[i + 1] == game_board[i + 2] == "O":
+            return "O"
+    for i in range(0, 3):
+        if game_board[i] == game_board[i + 3] == game_board[i + 6] == "X":
+            return "X"
+        elif game_board[i] == game_board[i + 3] == game_board[i + 6] == "O":
+            return "O"
+    if game_board[0] == game_board[4] == game_board[8] == "X":
+        return "X"
+    elif game_board[0] == game_board[4] == game_board[8] == "O":
+        return "O"
+    if game_board[2] == game_board[4] == game_board[6] == "X":
+        return "X"
+    elif game_board[2] == game_board[4] == game_board[6] == "O":
+        return "O"
+
+    for cell in game_board:
+        if cell == "":
+            return "No"
+    else:
+        return "Stalemate"
 
 
 def game_loop(win: pygame.Surface) -> None:
+    font = pygame.font.SysFont('comicsans', 60, False)
+    player_x = font.render("Player X Turn", 1, X_COLOR)
+    player_o = font.render("Player O Turn", 1, O_COLOR)
+    x_winner = font.render("Player X WINS!!", 1, WINNER_COLOR)
+    o_winner = font.render("Player O WINS!!", 1, WINNER_COLOR)
+    stalemate = font.render("Stalemate", 1, STALEMATE_COLOR)
+    game_over = font.render("Game Over", 1, GAME_OVER_COLOR)
+
+    game_board = ["" for _ in range(9)]
+    turn = 0
     run = True
 
     while run:
         draw_grid(win)
-        pygame.display.update()
+        if turn == 0:
+            pygame.draw.rect(win, (0, 0, 0), (40, 400, 300, 80))
+            win.blit(player_x, (60, 420))
+        elif turn == 1:
+            pygame.draw.rect(win, (0, 0, 0), (40, 400, 300, 80))
+            win.blit(player_o, (60, 420))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -70,14 +107,34 @@ def game_loop(win: pygame.Surface) -> None:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 grid_pos = get_grid_location(mouse_x, mouse_y)
                 if grid_pos:
-                    draw_x(win, *grid_pos)
+                    if turn == 0 and game_board[grid_pos - 1] == "":
+                        draw_x(win, *POS_DICT[grid_pos])
+                        game_board[grid_pos - 1] = "X"
+                        turn = 1
+                    elif turn == 1 and game_board[grid_pos - 1] == "":
+                        draw_o(win, *POS_DICT[grid_pos])
+                        game_board[grid_pos - 1] = "O"
+                        turn = 0
+        state = check_for_winner(game_board)
+        if state == "No":
+            pass
+        else:
+            pygame.draw.rect(win, (0, 0, 0), (40, 400, 300, 90))
+            win.blit(game_over, (85, 400))
+            if state == "X":
+                win.blit(x_winner, (43, 450))
+            elif state == "O":
+                win.blit(o_winner, (43, 450))
+            elif state == "Stalemate":
+                win.blit(stalemate, (98, 450))
+
+        pygame.display.update()
 
 
 def main() -> None:
     pygame.init()
     win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Tic Tac Toe")
-    # font = pygame.font.SysFont('comicsans', 26, True)
     game_loop(win)
 
 
